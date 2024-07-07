5b0eb587c1eaa86d3d88262ec3854b94d0ca0e58
Sandesh More <34198712+sandeshlmore@users.noreply.github.com>
2024-07-07 10:12:24 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+
+#Labour’s victory was seismic. It was very nearly unprecedented; only Tony Blair’s Labour Party has ever won more seats in an election.
+
+#Addressing the nation before entering 10 Downing Street on Friday afternoon, Starmer pledged a return to the politics of public service, vowing to heal the “weariness at the heart of the nation” with “action not words.”
+
+#But Labour’s win was also fragile. The vote breakdown made clear that the election was as much, if not more, about the public’s anger towards the Conservatives as it was about excitement for Labour’s offer.
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
